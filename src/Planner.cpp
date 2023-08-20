c1029bf239138eede9d44c8c3671cc61c6f066e9
YotamAmrani <yotamcdo@gmail.com>
2023-08-20 16:18:39 +0300

    modified planner, pay attention to time interval!

--- src/Planner.cpp
+++ src/Planner.cpp
@@ -1,200 +1,107 @@
 #include "Planner.h"
 #include <Arduino.h>
-bool flag = false;
-Planner::Planner(StepperController *stepper_c, struct segment_plan *seg_pl)
+
+Planner::Planner(Stepper (&axis)[N_AXIS])
 {
-    stepper_c_ = stepper_c;
-    segment_plan_ = seg_pl;
+    axis_ = axis;
     current_drawing_ = nullptr;
     current_segment_ = 0;
     is_segment_printing_ = false;
     finished_drawing_ = true;
 }
 
-void Planner::print_stepper()
-{
-    const int *pos = stepper_c_->get_steps_count();
-    Serial.println("POS:");
-    Serial.print(pos[X_AXIS]);
-    Serial.print(",");
-    Serial.print(pos[Y_AXIS]);
-    Serial.print(",");
-    Serial.println(pos[Z_AXIS]);
-}
-
 void Planner::init_segment_plan(const int *target_pos)
 {
     Serial.println("Loading segment.");
-    // print_steps();
-    // const int *temp_position = stepper_c_->get_steps_count();
-    segment_plan_->current_position = stepper_c_->get_steps_count();
-    // segment_plan_->current_position[X_AXIS] = temp_position[X_AXIS];
-    // segment_plan_->current_position[Y_AXIS] = temp_position[Y_AXIS];
-    // segment_plan_->current_position[Z_AXIS] = temp_position[Z_AXIS];
-    segment_plan_->target_position[X_AXIS] = target_pos[X_AXIS];
-    segment_plan_->target_position[Y_AXIS] = target_pos[Y_AXIS];
-    segment_plan_->target_position[Z_AXIS] = target_pos[Z_AXIS];
-    segment_plan_->dx = abs(segment_plan_->current_position[X_AXIS] - target_pos[X_AXIS]);
-    segment_plan_->dy = abs(segment_plan_->current_position[Y_AXIS] - target_pos[Y_AXIS]);
-    segment_plan_->dz = abs(segment_plan_->current_position[Z_AXIS] - target_pos[Z_AXIS]);
-    segment_plan_->dominant_axis = max(max(segment_plan_->dx, segment_plan_->dy), segment_plan_->dz);
-    segment_plan_->x_step_value = 0;
-    segment_plan_->y_step_value = 0;
-    segment_plan_->z_step_value = 0;
-    // Set steps directions
-    segment_plan_->current_direction_mask = get_line_direction_mask(segment_plan_->current_position, segment_plan_->target_position);
-    segment_plan_->current_step_mask = 0;
-    // _segment_plan->segment_is_enabled = false;
+    float dx = abs(axis_[X_AXIS].get_steps_count() - target_pos[X_AXIS]);
+    float dy = abs(axis_[Y_AXIS].get_steps_count() - target_pos[Y_AXIS]);
+    float dz = abs(axis_[Z_AXIS].get_steps_count() - target_pos[Z_AXIS]);
+    float dominant_axis = max(max(dx, dy), dz);
+
+    // set directions
+    axis_[X_AXIS].set_direction(sgn(target_pos[X_AXIS] - axis_[X_AXIS].get_steps_count()));
+    axis_[Y_AXIS].set_direction(sgn(target_pos[Y_AXIS] - axis_[Y_AXIS].get_steps_count()));
+    axis_[Z_AXIS].set_direction(sgn(target_pos[Z_AXIS] - axis_[Z_AXIS].get_steps_count()));
+
+    // set steps_rate
+    int x_steps_rate = int((dominant_axis / dx) * STEPS_RATE);
+    int y_steps_rate = int((dominant_axis / dy) * STEPS_RATE);
+    int z_steps_rate = int((dominant_axis / dz) * STEPS_RATE);
+    axis_[X_AXIS].set_steps_rate(x_steps_rate);
+    axis_[Y_AXIS].set_steps_rate(y_steps_rate);
+    axis_[Z_AXIS].set_steps_rate(z_steps_rate);
+
+    // Serial.print("directions : ");
+    // Serial.print(sgn(target_pos[X_AXIS] - axis_[X_AXIS].get_steps_count()));
+    // Serial.print(",");
+    // Serial.print(sgn(target_pos[Y_AXIS] - axis_[Y_AXIS].get_steps_count()));
+    // Serial.print(",");
+    // Serial.println(sgn(target_pos[Z_AXIS] - axis_[Z_AXIS].get_steps_count()));
+
+    // Serial.print("directions : ");
+    // Serial.print(axis_[X_AXIS].get_direction());
+    // Serial.print(",");
+    // Serial.print(axis_[Y_AXIS].get_direction());
+    // Serial.print(",");
+    // Serial.println(axis_[Z_AXIS].get_direction());
+    // Serial.println("________________________");
+    // Serial.print("deltas : ");
+    // Serial.print((dominant_axis / dx));
+    // Serial.print(",");
+    // Serial.print(dominant_axis / dy);
+    // Serial.print(",");
+    // Serial.println(dominant_axis / dz);
+    // Serial.println("________________________");
+    Serial.print("steps rate : ");
+    Serial.print(x_steps_rate);
+    Serial.print(",");
+    Serial.print(y_steps_rate);
+    Serial.print(",");
+    Serial.println(z_steps_rate);
+    Serial.println("________________________");
 }
 
-void Planner::print_segment()
+void Planner::move_to_position(const int *target_position)
 {
-    Serial.print("current step mask: ");
-    Serial.print(segment_plan_->current_step_mask);
-    Serial.print(",");
-    Serial.println(segment_plan_->current_direction_mask);
-    Serial.print("current pos: ");
-    Serial.print(segment_plan_->current_position[X_AXIS]);
-    Serial.print(",");
-    Serial.print(segment_plan_->current_position[Y_AXIS]);
-    Serial.print(",");
-    Serial.println(segment_plan_->current_position[Z_AXIS]);
+    const int current_position[N_AXIS] = {
+        axis_[X_AXIS].get_steps_count(),
+        axis_[Y_AXIS].get_steps_count(),
+        axis_[Z_AXIS].get_steps_count(),
+    };
+    // Print position
+    // Serial.print("Current position: ");
+    // Serial.print(current_position[X_AXIS]);
+    // Serial.print(",");
+    // Serial.print(current_position[Y_AXIS]);
+    // Serial.print(",");
+    // Serial.println(current_position[Z_AXIS]);
+    // Serial.println("________________________");
 
-    Serial.print("target pos: ");
-    Serial.print(segment_plan_->target_position[X_AXIS]);
-    Serial.print(",");
-    Serial.print(segment_plan_->target_position[Y_AXIS]);
-    Serial.print(",");
-    Serial.println(segment_plan_->target_position[Z_AXIS]);
-    Serial.print("deltas: ");
-    Serial.print(segment_plan_->dx);
-    Serial.print(",");
-    Serial.print(segment_plan_->dy);
-    Serial.print(",");
-    Serial.print(segment_plan_->dz);
-    Serial.print(",");
-    Serial.println(segment_plan_->dominant_axis);
-    Serial.print("step values : ");
-    Serial.print(segment_plan_->x_step_value);
-    Serial.print(",");
-    Serial.print(segment_plan_->y_step_value);
-    Serial.print(",");
-    Serial.println(segment_plan_->z_step_value);
-
-    Serial.println("--------------------------");
-}
-
-void Planner::print_steps()
-{
-    Serial.println("steps values:");
-    Serial.print(segment_plan_->x_step_value);
-    Serial.print(",");
-    Serial.print(segment_plan_->y_step_value);
-    Serial.print(",");
-    Serial.println(segment_plan_->z_step_value);
-    Serial.println("_________________________________");
-}
-
-void Planner::print_segment_positions()
-{
-    Serial.print("current pos: ");
-    Serial.print(segment_plan_->current_position[X_AXIS]);
-    Serial.print(",");
-    Serial.print(segment_plan_->current_position[Y_AXIS]);
-    Serial.print(",");
-    Serial.println(segment_plan_->current_position[Z_AXIS]);
-    Serial.print("target pos: ");
-    Serial.print(segment_plan_->target_position[X_AXIS]);
-    Serial.print(",");
-    Serial.print(segment_plan_->target_position[Y_AXIS]);
-    Serial.print(",");
-    Serial.println(segment_plan_->target_position[Z_AXIS]);
-    Serial.println("______________________________________");
-}
-
-void Planner::move_to_position()
-{
-    const int *current_position = stepper_c_->get_steps_count();
-    // if (
-    //     segment_plan_->current_position[X_AXIS] != current_position[X_AXIS] ||
-    //     segment_plan_->current_position[Y_AXIS] != current_position[Y_AXIS] ||
-    //     segment_plan_->current_position[Z_AXIS] != current_position[Z_AXIS])
-    // {
-    //     Serial.println("WTF");
-    // }
-    if (current_position[X_AXIS] != segment_plan_->target_position[X_AXIS] ||
-        current_position[Y_AXIS] != segment_plan_->target_position[Y_AXIS] ||
-        current_position[Z_AXIS] != segment_plan_->target_position[Z_AXIS])
+    /** START PRINTING **/
+    if (!is_segment_printing_)
     {
-        /** START PRINTING **/
-        if (!is_segment_printing_)
-        {
-            is_segment_printing_ = true;
-            stepper_c_->set_enable(true);
-            stepper_c_->set_direction(segment_plan_->current_direction_mask);
-        }
+        is_segment_printing_ = true;
+    }
 
-        segment_plan_->current_step_mask = 0;
-        // Extract X step value
-        if (current_position[X_AXIS] != segment_plan_->target_position[X_AXIS])
-        {
-            if (segment_plan_->dx && (segment_plan_->x_step_value >= segment_plan_->dominant_axis))
-            {
-                segment_plan_->current_step_mask = segment_plan_->current_step_mask | (1 << X_AXIS);
-                segment_plan_->x_step_value -= segment_plan_->dominant_axis;
-            }
-            segment_plan_->x_step_value += segment_plan_->dx;
-        }
-        // Extract Y step value
-        if (current_position[Y_AXIS] != segment_plan_->target_position[Y_AXIS])
-        {
-            if (segment_plan_->dy && (segment_plan_->y_step_value >= segment_plan_->dominant_axis))
-            {
-                segment_plan_->current_step_mask = segment_plan_->current_step_mask | (1 << Y_AXIS);
-                segment_plan_->y_step_value -= segment_plan_->dominant_axis;
-            }
-            segment_plan_->y_step_value += segment_plan_->dy;
-        }
-        // Extract Z step value
-        if (current_position[Z_AXIS] != segment_plan_->target_position[Z_AXIS])
-        {
-            if (segment_plan_->dz && (segment_plan_->z_step_value >= segment_plan_->dominant_axis))
-            {
-                segment_plan_->current_step_mask = segment_plan_->current_step_mask | (1 << Z_AXIS);
-                segment_plan_->z_step_value -= segment_plan_->dominant_axis;
-            }
-            segment_plan_->z_step_value += segment_plan_->dz;
-        }
+    if (current_position[X_AXIS] != target_position[X_AXIS] ||
+        current_position[Y_AXIS] != target_position[Y_AXIS] ||
+        current_position[Z_AXIS] != target_position[Z_AXIS])
+    {
 
-        stepper_c_->move_step(segment_plan_->current_step_mask, segment_plan_->current_direction_mask);
+        for (int i = 0; i < N_AXIS; ++i)
+        {
+            if (current_position[i] != target_position[i])
+            {
+                axis_[i].move_step(axis_[i].get_direction());
+            }
+        }
     }
     else
     {
-        // Serial.println("got here");
-        stepper_c_->set_enable(false);
+        /** END PRINTING **/
         is_segment_printing_ = false;
+        Serial.println("Finished segment!");
     }
-}
-
-int Planner::get_line_direction_mask(const int *point1, const int *point2)
-{
-    const int x_direction_sign = sgn(point2[X_AXIS] - point1[X_AXIS]);
-    const int y_direction_sign = sgn(point2[Y_AXIS] - point1[Y_AXIS]);
-    const int z_direction_sign = sgn(point2[Z_AXIS] - point1[Z_AXIS]);
-    int current_direction_mask = 0;
-    if (x_direction_sign && x_direction_sign < 0)
-    {
-        current_direction_mask = current_direction_mask | (1 << X_AXIS);
-    }
-    if (y_direction_sign && y_direction_sign < 0)
-    {
-        current_direction_mask = current_direction_mask | (1 << Y_AXIS);
-    }
-    if (z_direction_sign && z_direction_sign < 0)
-    {
-        current_direction_mask = current_direction_mask | (1 << Z_AXIS);
-    }
-    return current_direction_mask;
 }
 
 void Planner::load_drawing(double drawing_to_plot[][N_AXIS], int drawing_size)
@@ -204,49 +111,48 @@ void Planner::load_drawing(double drawing_to_plot[][N_AXIS], int drawing_size)
     finished_drawing_ = false;
     current_drawing_ = drawing_to_plot;
     current_drawing_size_ = drawing_size;
-    stepper_c_->set_steps_rate(700);
 }
 
-void Planner::plot_drawing()
-{
-    if (!finished_drawing_)
-    {
-        if (!is_segment_printing_ && current_segment_ < current_drawing_size_)
-        {
-            // Converting current target position into steps
-            // this is a local variable, it may be flushed after a while?
-            const int target_to_steps[N_AXIS] = {
-                int(mm_to_steps(X_STEPS_PER_MM, current_drawing_[current_segment_][X_AXIS])),
-                int(mm_to_steps(Y_STEPS_PER_MM, current_drawing_[current_segment_][Y_AXIS])),
-                int(mm_to_steps(Z_STEPS_PER_MM, current_drawing_[current_segment_][Z_AXIS]))};
+// void Planner::plot_drawing()
+// {
+//     if (!finished_drawing_)
+//     {
+//         if (!is_segment_printing_ && current_segment_ < current_drawing_size_)
+//         {
+//             // Converting current target position into steps
+//             // this is a local variable, it may be flushed after a while?
+//             const int target_to_steps[N_AXIS] = {
+//                 int(mm_to_steps(X_STEPS_PER_MM, current_drawing_[current_segment_][X_AXIS])),
+//                 int(mm_to_steps(Y_STEPS_PER_MM, current_drawing_[current_segment_][Y_AXIS])),
+//                 int(mm_to_steps(Z_STEPS_PER_MM, current_drawing_[current_segment_][Z_AXIS]))};
 
-            // start of segment
-            // init_segment_plan(drawing_to_plot[current_segment_]);
-            init_segment_plan(target_to_steps);
-            // print_segment_positions();
-            // print_segment();
-            move_to_position();
-            Serial.print("Finished segment: ");
-            Serial.println(current_segment_);
-            print_stepper();
-            current_segment_++;
-        }
-        else if (is_segment_printing_)
-        {
-            move_to_position();
-        }
-        else if (current_segment_ == current_drawing_size_)
-        {
-            print_stepper();
-            Serial.println("Finished drawing!");
-            current_drawing_ = nullptr;
-            current_drawing_size_ = 0;
-            current_segment_ = 0;
-            finished_drawing_ = true;
-            is_segment_printing_ = false;
-        }
-    }
-}
+//             // start of segment
+//             // init_segment_plan(drawing_to_plot[current_segment_]);
+//             init_segment_plan(target_to_steps);
+//             // print_segment_positions();
+//             // print_segment();
+//             move_to_position();
+//             Serial.print("Finished segment: ");
+//             Serial.println(current_segment_);
+//             print_stepper();
+//             current_segment_++;
+//         }
+//         else if (is_segment_printing_)
+//         {
+//             move_to_position();
+//         }
+//         else if (current_segment_ == current_drawing_size_)
+//         {
+//             print_stepper();
+//             Serial.println("Finished drawing!");
+//             current_drawing_ = nullptr;
+//             current_drawing_size_ = 0;
+//             current_segment_ = 0;
+//             finished_drawing_ = true;
+//             is_segment_printing_ = false;
+//         }
+//     }
+// }
 
 bool Planner::is_drawing_finished()
 {
--- src/Planner.h
+++ src/Planner.h
@@ -1,48 +1,38 @@
 #ifndef PLANNER_H
 #define PLANNER_H
-#include "StepperController.h"
+#include "Stepper.h"
 
 /** segment -represents the current line to plot **/
 struct segment_plan
 {
-    const int *current_position;
     int target_position[N_AXIS];
-    int current_step_mask;
-    int current_direction_mask;
-    int dx;
-    int dy;
-    int dz;
+    int current_step_mask[N_AXIS];
     int dominant_axis;
-    int x_step_value;
-    int y_step_value;
-    int z_step_value;
 };
 
 /** A Planner for handling line plotting in a Non-Blocking manner **/
 class Planner
 {
 private:
-    StepperController *stepper_c_;
-    struct segment_plan *segment_plan_;
+    Stepper *axis_;
     double (*current_drawing_)[N_AXIS];
     // https://stackoverflow.com/questions/22975633/reference-to-a-static-2d-array
     int current_drawing_size_;
     int current_segment_;
-
     bool finished_drawing_;
 
 public:
     bool is_segment_printing_;
-    Planner(StepperController *stepper_c, struct segment_plan *seg_pl);
-    void print_stepper();
-    void print_segment();
-    void print_steps();
-    void print_segment_positions();
+    Planner(Stepper (&axis)[N_AXIS]);
+    // void print_stepper();
+    // void print_segment();
+    // void print_steps();
+    // void print_segment_positions();
     void init_segment_plan(const int *target_pos);
     void load_drawing(double drawing_to_plot[][N_AXIS], int drawing_size);
-    void move_to_position();
-    int get_line_direction_mask(const int *point1, const int *point2);
-    void plot_drawing();
+    void move_to_position(const int *target_pos);
+    // int get_line_direction_mask(const int *point1, const int *point2);
+    // void plot_drawing();
     bool is_drawing_finished();
 };
 
